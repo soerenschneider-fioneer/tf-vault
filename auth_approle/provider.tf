3bba97edc88dcd621e7a16735f1a24b89acccd11
soeren <56670304+soerenschneider@users.noreply.github.com>
2022-05-01 14:48:54 +0200

    Lint

--- auth_approle/provider.tf
+++ auth_approle/provider.tf
@@ -2,7 +2,7 @@ terraform {
   experiments = [module_variable_optional_attrs]
   required_providers {
     vault = {
-      source  = "hashicorp/vault"
+      source = "hashicorp/vault"
     }
   }
 }
