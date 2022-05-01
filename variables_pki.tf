d0ffa0e37356a6b1d0ab015fd816301c6607aae9
soeren <56670304+soerenschneider@users.noreply.github.com>
2022-05-01 16:19:29 +0200

    Add variables for pki module

---
+++ variables_pki.tf
@@ -0,0 +1,31 @@
+variable "vault_server_url" {
+  type = string
+  validation {
+    condition     = can(regex("^http(s?)://", var.vault_server_url))
+    error_message = "The vault_server_url must be a valid HTTP url."
+  }
+}
+
+variable "pki_cert_domain" {
+  type = string
+}
+
+variable "pki_root_common_name" {
+  type = string
+}
+
+variable "pki_root_organization" {
+  type = string
+}
+
+variable "pki_im_common_name" {
+  type = string
+}
+
+variable "pki_im_organization" {
+  type = string
+}
+
+variable "pki_im_ou" {
+  type = string
+}
