c69729a358ee1833e8e654ffd3623bb5901612b4
soeren <56670304+soerenschneider@users.noreply.github.com>
2022-05-10 21:52:04 +0200

    update units

--- units.tf
+++ units.tf
@@ -1,6 +1,7 @@
 locals {
   default_3y_in_sec  = 94608000
   default_1y_in_sec  = 31536000
+  default_6mo_in_sec = 15768000
   default_1mo_in_sec = 2592000
   default_2w_in_sec  = 1209600
   default_1w_in_sec  = 604800
